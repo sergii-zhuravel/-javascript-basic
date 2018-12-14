d252478d737034b0bf5d3e33ffee5256de20bdfb
yurch-html2 <yurch-html@alterpost.org>
2018-12-14 11:54:41 +0200

    home work 2(edit)

--- roman_yurchenko/app.js
+++ roman_yurchenko/app.js
@@ -1,5 +1,5 @@
-var firstNumber =		prompt('add the first number');
-var secondNumber =	prompt('add the second number');
+var firstNumber =  prompt('add the first number');
+var secondNumber = prompt('add the second number');
 
 var sum = firstNumber * secondNumber;
 var message = '-- Сумма чисел =' + ' ' + sum + ' ' + '--';
