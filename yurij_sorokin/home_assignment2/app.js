da8d6a24ad0d6c65056fb1434519f8168133a72e
yuriy.sorokin <yuriy.sorokin.86@gmail.com>
2018-12-21 20:12:22 +0200

    home work 3

--- yurij_sorokin/home_assignment2/app.js
+++ yurij_sorokin/home_assignment2/app.js
@@ -1,4 +1,5 @@
 var num1 = parseInt(prompt('Please enter first number'));
+
 var num2 = parseInt(prompt('Please enter second number'));
   
 var total = num1 * num2;
---
+++ yurij_sorokin/home_assignment3/app.js
@@ -0,0 +1,14 @@
+var num = 1;
+var num2 = 1;
+
+
+num = num + num;
+num= num += 1;
+num = num + num2
+num = num + 1;
+num = num + (1*1);
+num = ++num;
+num = num + true;
+num = num + (!false);
+
+document.writeln(num);
