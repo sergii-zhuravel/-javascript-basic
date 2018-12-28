d71c3d3ab00dd09bc0e839c428ab4691d929c08e
Air <air@192.168.1.148.local.ipnet.ua>
2018-12-28 14:45:41 +0200

    Homeworks4_finished

---
+++ Vladymyr_Kulinich/home_assignment_5/app.js
@@ -0,0 +1,10 @@
+var todo = {
+    id: 123,
+    title: "maintenance",
+    description: "test_drive",
+    status: "activated"
+}
+
+for (key in todo) {
+    document.writeln.toUppercase (key + "=" + [key])
+}
