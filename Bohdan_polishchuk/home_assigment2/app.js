37657eb2cce8621d0039a52a4b776b7c6c883cac
Sergii <2285124+sergii-zhuravel@users.noreply.github.com>
2019-01-05 13:43:15 +0200

    Revert "Bohdan polishchuk"

--- Bohdan_polishchuk/home_assigment2/app.js
+++
@@ -1,4 +0,0 @@
-var numberOne = prompt("Enter first number", 0);
-var numberTwo = prompt("Enter second number", 0);
-var finishResult = parseInt(numberOne)*parseInt(numberTwo);
-document.write('-- Multiplication result = ', finishResult + ' --');
